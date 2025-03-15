4c9b713a355028bf7a28a1b739a4fe029afeb3c3
Azizbek <githubpython7@gmail.com>
2025-03-15 13:41:04 +0300

    1-dars

---
+++ 1.py
@@ -0,0 +1,8 @@
+import math
+x = 3
+y = 4
+z = 5
+a = (1 + math.sin(x + y)**2) / (2 + abs(x + (2 * x) / (1 + x**2 * y**2))) + x
+b = math.cos(math.atan(1 / z))**2
+print(f"a = {a}")
+print(f"b = {b}")
